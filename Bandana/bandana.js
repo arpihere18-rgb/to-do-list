90cda8b76a90ba89de50763c854805aabd3d1445
arpihere18-rgb <arpihere18@gmail.com>
2026-08-04 12:51:14 +0545

    My first repo

--- Bandana/bandana.js
+++ Bandana/bandana.js
@@ -1,45 +1,109 @@
-const input = document.getElementById("taskInput");
-const tasklist = document.getElementById("taskList");
+const input = document.getElementById("taskinput");  // select gareako input vanne lai 
+const tasklist = document.getElementById("tasklist");
+const Totaltask = document.getElementById("totaltask");
+const completedtask = document.getElementById("completedtask");
+
+let total = 0;
+let completed = 0;
 
 function addTask() {
-
-    let task = input.value;
+    let task = input.value   // task ko value yeta task ma store hunxa 
 
     if (task == "") {
-        alert("Please enter a task");
-        return;
+        alert("please enter the task");
+        return tasklist;    // khali vye pachi run hunna 
     }
 
-    let li = document.createElement("li");
-    li.innerText = task;
+
+    let li = document.createElement("li"); // create list item 
+    li.innerText = task;   // task bhitra ko value dinxa inner task le 
+
+
+    // create complete button 
 
     let completeBtn = document.createElement("button");
-    completeBtn.innerText = "Complete";
+    completeBtn.innerText = "complete";     // created the complete button 
     completeBtn.className = "completeBtn";
 
+
     let deleteBtn = document.createElement("button");
-    deleteBtn.innerText = "Delete";
+    deleteBtn.innerText = "delete";         // created the delete button 
     deleteBtn.className = "deleteBtn";
 
-    deleteBtn.onclick = function () {
-        li.remove();
-    };
+    let editBtn = document.createElement("button");
+    editBtn.innerText = "edit";
+    editBtn.className = "editBtn";
 
+
+    // add button inside Li 
     li.appendChild(completeBtn);
-    li.appendChild(deleteBtn);
+    li.appendChild(deleteBtn); // li is parent
+    li.appendChild(editBtn);
+
 
     tasklist.appendChild(li);
 
-    input.value = "";
+    total++;
+    Totaltask.innerText = total;
+
+
+
+    input.value = "";  // remove the input value once used 
+
 
     completeBtn.onclick = function () {
-        li.classList.toggle("done");
+
+        if (li.classList.contains("done")) {
+            li.classList.remove("done");
+            completed--;
+        } else {
+            li.classList.add("done");
+            completed++;
+        }
+
+        completedtask.innerText = completed;
     };
+
     deleteBtn.onclick = function () {
+
+        if (li.classList.contains("done")) {
+            completed--;
+            completedtask.innerText = completed;
+        }
+        total--;
+        totaltask.innerText = total;
+
         li.remove();
     };
 
-    function clearTask() {
-        tasklist.innerHTML = "";
+    editBtn.onclick = function () {
+        let newtask = prompt("edit your task", task);
+
+        if (newtask.trim() == "") {
+            alert("Task cannot be empty");
+            return;
+        }
+        task = newtask;
+        li.firstChild.textContent = task;
     }
+
+
+
+};
+function cleartsk() {
+    tasklist.innerHTML = "";
+    total = 0;
+    completed = 0;
+    totaltask.innerText = total;
+    completedtask.innerText = completed;
+
+
 }
+
+
+input.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+        addTask();
+    }
+
+});
